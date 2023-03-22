01994fdbd067297762c267d7d89e8345c4576ed2
Gabrielle de Vita <gabidevita18@gmail.com>
2023-03-22 20:52:47 -0300

    finalizando

--- assets/javascript/api.js
+++
@@ -1,7 +0,0 @@
-
-
-async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/projeto-base/data/profile.json';
-    const fetching = await fetch(url)
-    return await fetching.json()
-}
